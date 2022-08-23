1c0f681b3a37294c8e0222263276d808cbbc9c80
Camila Morlivo <morlivocamila01@gmail.com>
2022-08-22 21:22:14 -0300

    Clase 2 - Desafío 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import logo from './logo.jpg'
+import './NavBar.css';
+
+function App() {
+    return (
+        <header>
+            <img width="100px" height="80px" src={logo}/>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-12">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="">REGISTRARTE</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="">ENTRADAS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="">CONTACTO</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+
+export default App;
